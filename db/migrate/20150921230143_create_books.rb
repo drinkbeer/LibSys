64623e9b56f286b82e93ac116b2462793c9f8d89
drinkbeer <chenjb90@gmail.com>
2015-10-11 12:52:55 -0400

    add create_books.rb

--- db/migrate/20150921230143_create_books.rb
+++ db/migrate/20150921230143_create_books.rb
@@ -6,7 +6,7 @@ class CreateBooks < ActiveRecord::Migration
       t.string :author
       t.string :description
       t.string :status
-
+      t.string :receivers
       t.timestamps null: false
     end
   end
